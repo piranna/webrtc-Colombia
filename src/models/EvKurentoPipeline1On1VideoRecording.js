85392ae01fce84e69001c446c47880c6ca25e995
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-05-24 00:10:13 +0200

    Clean-up of `EvKurentoPipeline1On1VideoRecording`

--- src/models/EvKurentoPipeline1On1VideoRecording.js
+++ src/models/EvKurentoPipeline1On1VideoRecording.js
@@ -1,44 +1,36 @@
 class EvKurentoPipeline1On1VideoRecording{
+	_pipeline;
+	_candidates = {};
+	_sdpOffers = {};
+	_WebRtcEndPoints = {};
 
 	constructor(evKurentoClient,wss){
-		this._pipeline = {};
-		this._candidates = {};
-		this._sdpOffers = {};
 		this._evKurentoClient = evKurentoClient;
 		this._wss = wss;
-		this._WebRtcEndPoints = {};
-
-		this.addIceCandidate = this.addIceCandidate.bind(this);
-		this.releaseCandidatesPool = this.releaseCandidatesPool.bind(this);
-		this.generateSdpAnswer = this.generateSdpAnswer.bind(this);
-		this.startPipeline = this.startPipeline.bind(this);
-		this.generateSdpAnswer = this.generateSdpAnswer.bind(this);
-		this.releasePipeline = this.releasePipeline.bind(this);
-		this.setIceCandidates = this.setIceCandidates.bind(this);
-		this.setSdpOffers = this.setSdpOffers.bind(this);
 	}
-	addIceCandidate(clientId,candidate){
+
+	addIceCandidate = (clientId,candidate) => {
 		console.log('in addIceCandidate of pipeline file');
-		if (typeof this._candidates[clientId] == 'undefined' || this._candidates[clientId] == null || this._candidates[clientId].constructor.name != 'Array'){
+		if (!Array.isArray(this._candidates[clientId])){
 			_candidates[clientId] = [];
 		}
 		this._candidates[clientId].push(candidate);
 		return true;
 	}
-	releaseCandidatesPool(clientId){
-		this._candidates[clientId] = null;
-		Reflect.deleteProperty(this._candidates,clientId);
+
+	releaseCandidatesPool = (clientId) => {
 		delete this._candidates[clientId];
 		return true;
 	}
-	startPipeline(caller,callee,callback){
+
+	startPipeline = (caller,callee,callback) => {
 		let msg = {};
 		this._evKurentoClient.createPipeline((error,pl)=>{
 			if(error){
 				console.log(error);
-				callback(error);
-				return false;
+				return callback(error);
 			}
+
 			//console.log(pl);
 			//console.log("-------------------------");
 			//callback(null,this);
@@ -46,18 +38,19 @@ class EvKurentoPipeline1On1VideoRecording{
 			pl.create("WebRtcEndpoint",(error,callerWebRtcEndPoint)=>{
 				if (error){
 					console.log(error);
-					callback(error);
-					return false;
+					return callback(error);
 				}
+
 				// Gets the icecandidates already saved
 				//if (typeof this._candidates[caller.uid] != 'undefined' && typeof this._candidates[caller.uid] == 'Array'){
-				if (this._candidates[caller.uid]){	
-					while(this._candidates[caller.uid].length > 0){
+				if (this._candidates[caller.uid]){
+					while(this._candidates[caller.uid].length){
 						console.log('in while loop adding candidates for ' + caller.uid);
 						let candidate = this._candidates[caller.uid].shift();
 						callerWebRtcEndPoint.addIceCandidate(candidate);
 					}
 				}
+
 				// Defines a event handler for OnIceCandidate
 				console.log("Creating Caller WebRtcEndpoint...");
 				callerWebRtcEndPoint.on("OnIceCandidate",(event)=>{
@@ -72,23 +65,24 @@ class EvKurentoPipeline1On1VideoRecording{
 						candidate:candidate
 					}
 					this._wss.emmitMessageToSingleSocket ('message',msgObj,caller.socketid);
-
 				});
+
 				pl.create("WebRtcEndpoint",(error,calleeWebRtcEndPoint)=>{
 					if (error){
 						console.log(error);
-						callback(error);
-						return false;
+						return callback(error);
 					}
+
 					console.log("Creating Callee WebRtcEndpoint...");
 					//if (typeof this._candidates[callee.uid] != 'undefined' && typeof this._candidates[callee.uid] == 'Array'){
-					if (this._candidates[callee.uid]){	
-						while(this._candidates[callee.uid].length > 0){
+					if (this._candidates[callee.uid]){
+						while(this._candidates[callee.uid].length){
 							let candidate = this._candidates[callee.uid].shift();
 							console.log('in while loop adding candidates for ' + callee.uid);
 							calleeWebRtcEndPoint.addIceCandidate(candidate);
 						}
 					}
+
 					// Defines a event handler for OnIceCandidate
 					calleeWebRtcEndPoint.on("OnIceCandidate",(event)=>{
 						console.log(`A new ICE Candidate for callee has arrived, sending it out to ${callee.uid}`);
@@ -102,58 +96,61 @@ class EvKurentoPipeline1On1VideoRecording{
 							candidate:candidate
 						}
 						this._wss.emmitMessageToSingleSocket ('message',msgObj,callee.socketid);
-
 					});
+
 					//Connects the endpoints
 					callerWebRtcEndPoint.connect(calleeWebRtcEndPoint,(error)=>{
-                        if (error) {
-                            pl.release();
-                            return callback(error);
-                        }
+            if (error) {
+              pl.release();
+              return callback(error);
+            }
 
-                        calleeWebRtcEndPoint.connect(callerWebRtcEndPoint,(error)=>{
-                            if (error) {
-                                pl.release();
-                                return callback(error);
-                            }
-                        });
+            calleeWebRtcEndPoint.connect(callerWebRtcEndPoint,(error)=>{
+              if (error) {
+                pl.release();
+                return callback(error);
+              }
+            });
 
-                        this._pipeline = pl;
-                        this._WebRtcEndPoints[callee.uid] = calleeWebRtcEndPoint;
-                        this._WebRtcEndPoints[caller.uid] = callerWebRtcEndPoint;
-                        callback(null,this);
-                    });
+            this._pipeline = pl;
+            this._WebRtcEndPoints[callee.uid] = calleeWebRtcEndPoint;
+            this._WebRtcEndPoints[caller.uid] = callerWebRtcEndPoint;
+
+            callback(null,this);
+          });
 				});
-
 			});
-
 		});
 	}
+
 	//=====================================================
-	setSdpOffers(sdpOffers){
+	setSdpOffers = (sdpOffers) => {
 		this._sdpOffers = sdpOffers;
 	}
-	setIceCandidates(iceCandidates){
+
+	setIceCandidates = (iceCandidates) => {
 		this._candidates = iceCandidates;
 	}
 	//=====================================================
-	releasePipeline(){
-		if (typeof this._pipeline != 'undefined' && this._pipeline.constructor.name == 'MediaPipeline'){
+
+	releasePipeline = () => {
+		if (this._pipeline){
 			this._pipeline.release();
 		}
-		this._pipeline = {};
+
+		delete this._pipeline;
 		return true;
 	}
 
-	generateSdpAnswer(uid,callback){
+	generateSdpAnswer = (uid,callback) => {
 		this._WebRtcEndPoints[uid].processOffer(this._sdpOffers[uid],(error,sdpAnswer)=>{
 			console.log(`Generating sdpAnswer for ... ${uid}` );
-			
+
 			if (error){
 				console.log(error);
-				callback(error,sdpAnswer);
-				return false;
+				return callback(error,sdpAnswer);
 			}
+
 			callback (null,sdpAnswer);
 			// this._WebRtcEndPoints[uid].gatherCandidates((error)=>{
 			// 	console.log(`GatherCandidates for ${uid}...`);
@@ -163,15 +160,16 @@ class EvKurentoPipeline1On1VideoRecording{
 			// 	}
 			// });
 		});
+
 		this._WebRtcEndPoints[uid].gatherCandidates((error)=>{
 			console.log(`GatherCandidates for ${uid}...`);
-	        if (error) {
-	        	console.log(`Error gathering ICE Candidates for ${uid}`)
-	        	console.log(error);
-	        }
+      if (error) {
+      	console.log(`Error gathering ICE Candidates for ${uid}`)
+      	console.log(error);
+      }
 		});
-
-		
 	}
 }
-module.exports = exports = EvKurentoPipeline1On1VideoRecording;
+
+
+module.exports = EvKurentoPipeline1On1VideoRecording;
